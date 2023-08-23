23504e1f3017648acd9e9cf2603d2d4750f1ccb2
rune-encoder <132966351+rune-encoder@users.noreply.github.com>
2023-08-23 09:29:59 -0500

    Added feature to add new employee. Added function to automatically create manager list and role list on load to load up questions properly. Created new constructor function for the new feature

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,16 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
-const db = mysql.createConnection(
+const db = mysql
+  .createConnection(
     {
-        host: 'localhost',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+      host: "localhost",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     console.log(`Connected to the Employee CMS Database.`)
-).promise();
+  )
+  .promise();
 
-module.exports = db 
+module.exports = db;
